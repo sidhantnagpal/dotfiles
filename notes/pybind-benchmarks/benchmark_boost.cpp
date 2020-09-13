cbaa938287a3da3dcdff1acdc4007963d17f9408
Sidhant Nagpal <sidhantnagpal97@gmail.com>
2020-09-14 01:36:14 +0530

    Update macOS/Linux steps in README and document the roadblocks faced for Boost.Python

--- notes/pybind-benchmarks/benchmark_boost.cpp
+++ notes/pybind-benchmarks/benchmark_boost.cpp
@@ -22,8 +22,8 @@ BOOST_PYTHON_MODULE(benchmark_boost)
 {
     bp::def("get_string_hash", &twr::get_string_hash);
 
-    bp::class_<std::vector<uint32_t> >("Uint32Vec")
-        .def(bp::vector_indexing_suite<std::vector<uint32_t> >());
+    bp::class_<std::vector<uint32_t>>("Uint32Vec")
+        .def(bp::vector_indexing_suite<std::vector<uint32_t>>());
     bp::def("get_number_range", &twr::get_number_range);
 
     bp::class_<twr::ListenerWrap, boost::noncopyable>("Listener", bp::init<>())
